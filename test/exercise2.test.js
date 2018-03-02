8e44cbd8fe86d8d61aab186d56a106e9a0948f75
Steven Fongue <sfongue@gmail.com>
2018-03-02 10:09:22 +0100

    Exercice 2

--- test/exercise2.test.js
+++ test/exercise2.test.js
@@ -5,7 +5,37 @@ describe(`${program.title}`, function () {
 
   it('Function yolo with mysterious parameter should return a list of messages', function () {
 
-    const whereIsMyMind = "???";
+    const whereIsMyMind = [
+      {
+        name: 'William',
+        sex: 1
+      },
+      {
+        name: 'Kara',
+        sex: 2
+      },
+      {
+        name: 'Laura',
+        sex: 2
+      },
+      {
+        name: 'Gaius',
+        sex: 1
+      },
+      {
+        name: 'Number six',
+        sex: 9
+      },
+      {
+        name: 'Lee',
+        sex: 1
+      },
+      {
+        name: 'Number height',
+        sex: 9
+      },
+
+    ];
 
     const output = [
       "0 not known character(s) ",
